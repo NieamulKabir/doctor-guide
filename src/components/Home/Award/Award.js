4e02b1c66fbbda36ef23da931f9f5638fd662547
NieamulKabir <kabir000079@gmail.com>
2023-06-20 23:21:16 +0600

    award

--- src/components/Home/Award/Award.js
+++ src/components/Home/Award/Award.js
@@ -11,27 +11,46 @@ const Award = () => {
                 <div className="hero h-[510px]" style={bgStyle}>
                     <div className="hero-overlay bg-opacity-60 bg-gradient-to-t from-[#4c07bcd4] to-[#ffffff05] "></div>
                     <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-md">
-                            <div className='grid grid-cols-3 gap-10'>
-                                <div className='flex items-center'>
-                                    <div>
-                                        <p className='text-7xl'>
-                                            <i className="fa-solid fa-user-doctor"></i>
-                                        </p>
-                                        <h1 className='text-[40px] font-bold'>50</h1>
+                        <div className='flex items-center justify-evenly'>
 
-                                    </div>
-                                    <div>
 
-                                    </div>
-                                    <div>
-
-                                    </div>
-                                    <div>
-
-                                    </div>
-                                </div>
+                            <div className='mr-10'>
+                                <p className='text-7xl'>
+                                    <i className="fa-solid fa-user-doctor"></i>
+                                </p>
+                                <h1 className='text-[40px] font-bold'>50</h1>
+                                <hr h-10 />
+                                <h1 className='text-[32px] font-bold'>Medical Experts</h1>
                             </div>
+
+                            <div className='mr-10'>
+                                <p className='text-7xl'>
+                                    <i className="fa-solid fa-bed"></i>
+                                </p>
+                                <h1 className='text-[40px] font-bold'>500</h1>
+                                <hr h-10 />
+                                <h1 className='text-[32px] font-bold'>Hospital Rooms</h1>
+
+                            </div>
+
+                            <div className='mr-10'>
+                                <p className='text-7xl'>
+                                <i className="fa-solid fa-trophy"></i>
+                                </p>
+                                <h1 className='text-[40px] font-bold'>20</h1>
+                                <hr h-10 />
+                                <h1 className='text-[32px] font-bold'>Awards Win</h1>
+                            </div>
+
+                            <div className='mr-10'>
+                                <p className='text-7xl'>
+                                <i className="fa-solid fa-face-smile"></i>
+                                </p>
+                                <h1 className='text-[40px] font-bold'>5000</h1>
+                                <hr h-10 />
+                                <h1 className='text-[32px] font-bold'>Happy Patients</h1>
+                            </div>
+
                         </div>
                     </div>
                 </div>
--- src/components/Home/HeroSection/HeroSection.js
+++ src/components/Home/HeroSection/HeroSection.js
@@ -7,61 +7,36 @@ import doctor2 from '../../utils/image/doctorImg/doctor2.png'
 const HeroSection = () => {
     return (
         <div className="relative">
-        <Circle/>
-        <div className="my-container grid md:grid-cols-3 md:ms-12 gap-4 relative">
-            <div className="flex items-center relative">
-                <div className="absolute top-0"></div>
-                <div className='ml-10'>
-                    <h2 className="text-lg md:text-[40px] font-bold">Find The Care <br /> <br /> You Need </h2>
-                    <p className="text-[14px] py-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry is standard dummy text ever since the 1500s,</p>
-                    <button className='font-medium bg-[#FF8B42] px-10 py-4 rounded-lg text-white'>Make Appointment</button>
+            <Circle />
+            <div className="my-container grid md:grid-cols-3 md:ms-12 gap-4 relative">
+                <div className="flex items-center relative">
+                    <div className="absolute top-0"></div>
+                    <div className='ml-10'>
+                        <h2 className="text-lg md:text-[40px] font-bold">Find The Care <br /> <br /> You Need </h2>
+                        <p className="text-[14px] py-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry is standard dummy text ever since the 1500s,</p>
+                        <button className='font-medium bg-[#FF8B42] px-10 py-4 rounded-lg text-white'>Make Appointment</button>
+                    </div>
                 </div>
-            </div>
 
-            <div className="relative mb-14 mt-8 ms-auto">
-                <img className="absolute bottom-0 md:h-full pt-[100px]" src={doctor1} alt="" />
-                <div className="absolute bottom-[-56px] left-[-60px]"></div>
-                <div className="flex flex-col justify-between h-full">
-                    <div className="h-[177px] mb-14 w-[338] bg-[#FF8B42] rounded-b-[150px] rounded-t-[20px] mt-[70px]"></div>
-                    <div className="h-[434px] w-[338px] bg-[#FF8B42] rounded-t-[150px] rounded-b-[20px]"></div>
+                <div className="relative mb-14 mt-8 ms-auto">
+                    <img className="absolute bottom-0 md:h-full pt-[100px]" src={doctor1} alt="" />
+                    <div className="absolute bottom-[-56px] left-[-60px]"></div>
+                    <div className="flex flex-col justify-between h-full">
+                        <div className="h-[177px] mb-14 w-[338] bg-[#FF8B42] rounded-b-[150px] rounded-t-[20px] mt-[70px]"></div>
+                        <div className="h-[434px] w-[338px] bg-[#FF8B42] rounded-t-[150px] rounded-b-[20px]"></div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="ms-auto mr-[110px] relative">
-                <img className="bottom-0 h-[792px] absolute" src={doctor2} alt="" />
-                <div className="flex flex-col justify-between h-full">
-                    <div className="h-[434px] w-[338px] bg-[#0089BA] rounded-b-[150px] rounded-t-[20px] mt-[150px]"></div>
-                    <div className="h-[177px] w-[338] bg-[#0089BA] rounded-t-[150px] rounded-b-[20px]"></div>
+                <div className="ms-auto mr-[110px] relative">
+                    <img className="bottom-0 h-[792px] absolute" src={doctor2} alt="" />
+                    <div className="flex flex-col justify-between h-full">
+                        <div className="h-[434px] w-[338px] bg-[#0089BA] rounded-b-[150px] rounded-t-[20px] mt-[150px]"></div>
+                        <div className="h-[177px] w-[338] bg-[#0089BA] rounded-t-[150px] rounded-b-[20px]"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-        // <div className='grid md:grid-cols-3 md:ms-12 gap-4'>
-        //     <div className='ml-16 pb-10 items-center mx-auto'>
-        //         <div className='2xl:mr-60 md:flex row  mx-auto items-center '>
-        //             <div className=' mt-64 '>
-        //                 <h1 className='text-lg md:text-[40px] font-bold '>Find The Care <br /> <br />You Need</h1>
-        //                 <p className='text-[14px] '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        //                 <button className='font-medium bg-[#FF8B42] px-10 py-4 rounded-lg text-white'>Make Appointment</button>
-        //             </div>
-        //             <div className=''>
-        //                 <div className=' '>
-        //                     <div className='shape1'></div>
-        //                     <div className='shape2'></div>
-        //                     <div className='doctor1' ></div>
 
-        //                 </div>
-        //                 <div className=''>
-        //                     <div className='shape4'></div>
-        //                     <div className='shape3'></div>
-        //                     <div className='doctor2' ></div>
-        //                 </div>
-        //             </div>
-
-        //         </div>
-        //         <br /> <br /> <br />
-        //     </div>
-        // </div>
 
     );
 };
